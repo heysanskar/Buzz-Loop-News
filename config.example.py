774f5c5c494607fba3b85882d4444488019cd26d
Sanskar <245161751+heysanskar@users.noreply.github.com>
2026-09-17 23:09:38 +0530

    config.example.py

--- config.example.py
+++ config.example.py
@@ -1,6 +1,6 @@
 SENDER_EMAIL = "your-email@gmail.com"
 SENDER_PASSWORD = "your-gmail-app-password"
-RECEIVER_EMAIL = "receivers-email@gmail.com"
+RECEIVER_EMAIL = "receiver-email@gmail.com"
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
